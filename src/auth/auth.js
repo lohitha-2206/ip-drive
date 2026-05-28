0155bbc4c272b3a1c4105a051297a6fc1741f5bc
Savvana Lohitha <lohithasavvana@gmail.com>
2026-05-28 10:59:15 +0530

    Delete src directory

--- src/auth/auth.js
+++
@@ -1,4 +0,0 @@
-export const setUser = (user) =>
-  localStorage.setItem("user", JSON.stringify(user));
-export const getUser = () => JSON.parse(localStorage.getItem("user"));
-export const logout = () => localStorage.removeItem("user");
